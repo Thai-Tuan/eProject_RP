ed3158797840e4a6a6ce74cf7b8e5993fdad290a
Thai-Tuan <nttuana24083@cusc.ctu.edu.vn>
2024-12-26 10:26:09 +0700

    delete homejs

--- home.js
+++
@@ -1,5 +0,0 @@
-importScripts
-
-document.getElementById("product1").addEventListener("click", function() {
-    window.location.href = "http://127.0.0.1:5500/product1.html"; // Thay "https://example.com" bằng URL bạn muốn
-});
