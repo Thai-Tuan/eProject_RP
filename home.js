bf3e64e17572a40449c2a56d9a76d9057e800a3b
Thai-Tuan <nttuana24083@cusc.ctu.edu.vn>
2024-12-26 10:19:28 +0700

    Project RP

---
+++ home.js
@@ -0,0 +1,5 @@
+importScripts
+
+document.getElementById("product1").addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/product1.html"; // Thay "https://example.com" bằng URL bạn muốn
+});
